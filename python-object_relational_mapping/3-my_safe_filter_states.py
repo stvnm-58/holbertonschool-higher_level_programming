5fd93c489e01d842d03ea924efe4cd0a4877eb2e
steven <12992@holbertonstudents.com>
2026-06-12 12:26:00 +0200

    t3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Liste les états correspondant à l'argument, sécurisé contre l'injection SQL."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    cursor.execute(query, (state_name_searched,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
