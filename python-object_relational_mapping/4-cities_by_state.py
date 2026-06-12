c189d1c3b6ad693a247206f111bdaaf6d1c29c1a
steven <12992@holbertonstudents.com>
2026-06-12 12:57:10 +0200

    t4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Liste toutes les villes de la base de données hbtn_0e_4_usa."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="127.0.0.1",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
