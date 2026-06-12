a87e8d1d0000188d849d724027dc0e0187f89ac1
steven <12992@holbertonstudents.com>
2026-06-12 11:27:40 +0200

    correction t1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Filtre les états par l'argument fourni, protégé contre les injections SQL."""
+"""Liste tous les états commençant par la lettre N majuscule."""
 import sys
 import MySQLdb
 
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name_searched = sys.argv[4]
 
     # Connexion à la base de données
     db = MySQLdb.connect(
@@ -20,12 +19,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # Utilisation de %s pour la requête préparée (protection injection SQL)
-    # Le mot-clé BINARY force la sensibilité à la casse (Case Sensitivity)
-    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
-    
-    # On passe la variable dans un tuple (state_name_searched,) à l'execute
-    cursor.execute(query, (state_name_searched,))
+    # Requête SQL avec filtre sur le 'N' majuscule
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
     
     rows = cursor.fetchall()
 
