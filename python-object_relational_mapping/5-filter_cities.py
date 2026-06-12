0e01ab2701252e6a1066a2fb270ddb7c06d795b5
steven <12992@holbertonstudents.com>
2026-06-12 17:49:36 +0200

    correction t5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Liste toutes les villes en argument depuis la base hbtn_0e_4_usa."""
+"""Répertorie toutes les villes de la base de données."""
 import sys
 import MySQLdb
 
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
+    state_name_searched = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
@@ -18,20 +18,18 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = (
-        "SELECT cities.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id ASC"
-    )
-
-    cursor.execute(query, (state_name,))
-
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(query, (state_name_searched))
     rows = cursor.fetchall()
-    names = [row[0] for row in rows]
-    if names:
-        print(', '.join(names))
+
+    cities_list = [row[0] for row in rows]
+    print(", ".join(cities_list))
 
     cursor.close()
     db.close()
