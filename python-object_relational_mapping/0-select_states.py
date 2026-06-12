b3f79cbe393b431cdc73673d1ba5b09a0158910e
steven <12992@holbertonstudents.com>
2026-06-12 11:08:28 +0200

    correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,31 +4,24 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # On vérifie que les 3 arguments requis sont bien présents
-    if len(sys.argv) >= 4:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-        # Connexion à la base de données
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-        cursor = db.cursor()
-        
-        # Exécution et récupération
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cursor.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-        # Affichage
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
 
-        # Fermeture des connexions
-        cursor.close()
-        db.close()
+    cursor.close()
+    db.close()
