63079f9f2b586318cff7ccdf69aff61bed470376
steven <12992@holbertonstudents.com>
2026-06-12 17:45:37 +0200

    correction t5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Liste toutes les villes d'un état passé en argument depuis la base hbtn_0e_4_usa."""
+"""Liste toutes les villes en argument depuis la base hbtn_0e_4_usa."""
 import sys
 import MySQLdb
 
