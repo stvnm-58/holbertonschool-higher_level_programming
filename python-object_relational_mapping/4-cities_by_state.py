be45d31b576a61f42ac62715e9cb0809bb03f19c
steven <12992@holbertonstudents.com>
2026-06-12 13:42:09 +0200

    test code t4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -11,23 +11,23 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=username,
+        passwd=password,
+        db=database
     )
 
-    cur = db.cursor()
+    cursor = db.cursor()
     query = """
     SELECT cities.id, cities.name, states.name
     FROM cities
     JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC
     """
-    cur.execute(query)
+    cursor.execute(query)
     rows = cur.fetchall()
 
     for row in rows:
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
