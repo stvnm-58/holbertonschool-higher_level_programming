b1f026fce87bbf4297aa5280837d6eba9d7b8662
steven <12992@holbertonstudents.com>
2026-06-13 11:28:51 +0200

    t9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,13 +6,18 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: {} mysql_username mysql_password database_name".format(
+            sys.argv[0]
+        ))
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     # Connexion à la base de données
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
