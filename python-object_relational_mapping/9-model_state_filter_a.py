ae5cd1a1eaea3a43d1352e91e23767f9cebc0cdb
steven <12992@holbertonstudents.com>
2026-06-13 11:27:17 +0200

    t9

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-
+"""Liste tous les objets State contenant la lettre 'a' via SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-
-engine = create_engine(
+    # Connexion à la base de données
+    engine = create_engine(
         'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
     )
 
+    # Création de la session
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Requête avec filtre pour trouver la lettre 'a' et tri par ID
     states_with_a = session.query(State)\
         .filter(State.name.like('%a%'))\
         .order_by(State.id)\
         .all()
 
+    # Affichage des résultats
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
 
+    # Fermeture de la session
     session.close()
