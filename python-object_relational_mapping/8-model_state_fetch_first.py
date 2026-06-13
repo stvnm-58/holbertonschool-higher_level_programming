c75b1cf2a95154c44dde16b87adce53ab5a02f6e
steven <12992@holbertonstudents.com>
2026-06-13 11:21:06 +0200

    correction t8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,26 +10,19 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connexion à la base de données avec SQLAlchemy
     engine = create_engine(
         'mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(
             username, password, database
         ),
         pool_pre_ping=True
     )
-
-    # Création de la session de travail
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Récupération du premier State trié par id (.first() limite la requête SQL à 1 résultat)
     first_state = session.query(State).order_by(State.id).first()
 
-    # Affichage du résultat selon les contraintes de l'énoncé
     if first_state is not None:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
 
-    # Fermeture de la session
     session.close()
