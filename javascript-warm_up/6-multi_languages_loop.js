85439ff67f13a4aeadc01e5e4c6e53b2cc1dbb68
steven <12992@holbertonstudents.com>
2026-07-01 14:12:33 +0200

    chmod

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const myLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-
 for (let i = 0; i < myLanguages.length; i++) {
     console.log(myLanguages[i]);
 }
