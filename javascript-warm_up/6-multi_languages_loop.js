97d47fd2cafb1d21a4b832190122faba49d201fd
steven <12992@holbertonstudents.com>
2026-07-01 14:43:50 +0200

    correction

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const myLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+const myLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myLanguages.length; i++) {
-    console.log(myLanguages[i]);
+  console.log(myLanguages[i]);
 }
