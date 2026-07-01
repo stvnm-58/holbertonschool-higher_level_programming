94edf5bdc016ab40315a68a8d40ebf8eeac5ee17
steven <12992@holbertonstudents.com>
2026-07-01 14:47:44 +0200

    correction

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
 const myVar = parseInt(process.argv[2]);
-
-
-if (isNaN(myVar) == true) {
-    console.log("Missing number of occurences");
-}
-
-else {
-    for (let i = 0; i < myVar; i++) {
-        console.log("C is fun");
-    }
+if (isNaN(myVar)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < myVar; i++) {
+    console.log('C is fun');
+  }
 }
