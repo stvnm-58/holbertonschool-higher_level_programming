61aab44c6e673b7d3e46befd62a9532589f7c07e
steven <12992@holbertonstudents.com>
2026-07-01 14:44:50 +0200

    correction

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-
-const myVar = parseInt(process.argv[2]);
-
-if (isNaN(myVar)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + myVar);
+const myPhrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const phrase of myPhrases) {
+  console.log(phrase);
 }
