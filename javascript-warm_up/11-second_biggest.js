e14084f13b8525ed9e2d5b82238155cf89c8d335
steven <12992@holbertonstudents.com>
2026-07-01 14:15:50 +0200

    correction

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 const args = process.argv.slice(2).map(Number);
-
 if (args.length <= 1) {
   console.log(0);
 } else {
   const uniqueSortedArgs = [...new Set(args)].sort((a, b) => b - a);
-
   if (uniqueSortedArgs.length > 1) {
     console.log(uniqueSortedArgs[1]);
   } else {
