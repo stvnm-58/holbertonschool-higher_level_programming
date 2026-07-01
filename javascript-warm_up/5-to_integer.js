b9d1881d7176976b2e7916d5cdbccda6818bcfd4
steven <12992@holbertonstudents.com>
2026-07-01 14:45:48 +0200

    correction

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const myPhrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const phrase of myPhrases) {
-  console.log(phrase);
+
+const myVar = parseInt(process.argv[2]);
+
+if (isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myVar);
 }
--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const myLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myLanguages.length; i++) {
-  console.log(myLanguages[i]);
+const myPhrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const phrase of myPhrases) {
+  console.log(phrase);
 }
