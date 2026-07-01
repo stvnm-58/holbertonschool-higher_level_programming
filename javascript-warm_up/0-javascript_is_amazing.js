67bcbf7d7e693da9147d9e0aee84aaed511d531c
steven <12992@holbertonstudents.com>
2026-07-01 13:33:05 +0200

    correction

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+// Shebang spécifiant que le script doit être exécuté avec Node.js
 
+// Déclaration d'une constante nommée myVar et affectation de la chaîne de caractères
 const myVar = "JavaScript is amazing";
+
+// Affichage du contenu de la variable myVar dans la console
 console.log(myVar);
